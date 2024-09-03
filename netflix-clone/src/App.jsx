a16a74c1959d60cf791b37b0c39fe9b5f16aeec2
K1ngJadon2012 <tdawit31@yahoo.com>
2024-09-03 14:11:40 -0700

    intial comit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  
+    <div>Hello</div>
+    <h2>This is Netflix Project that</h2>
+    </>
+  )
+}
+
+export default App
