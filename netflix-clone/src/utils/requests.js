da3d83a76f43fd27540fbb1a0a60049dcc7a6100
K1ngJadon2012 <tdawit31@yahoo.com>
2024-09-05 13:18:57 -0700

    Adding Banner and change on index.html

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,19 +1,37 @@
-const API_KEY = process.env.REACT_APP_API_KEY
-//"e48b1722d68d46e5996e073679a7a0f0";
-// https://api.themoviedb.org/3/movie/550?api_key=e48b1722d68d46e5996e073679a7a0f0
-
+const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
-
-
-fetchTrending: '/trending/all/week?api_key=${API_KEY}&language=en-US',
-fetchNetflixOriginals: '/discover/tv?api_key=${API_KEY}&with_networks=213',
-fetchTopRatedMovies: '/movie/top_rated?api_ key=S{API KEY}51anguagesen-US', 
-fetchActionMovies: '/discover/movie?api_key=${API_KEY}&with_genres=28', 
-fetchComedyMovies: '/discover/movie?api_key=${API_KEY}&with_genres=35',
-fetchHorrorMovies: '/discover/movie?ap_key=${API_KEY}Swith_genres=27', 
-fetchRomanceMovies: '/discover/movie?api_key=${API_KEY}&with_genres=10749', 
-fetchDocumentaries: '/discover/movie?ap1_key=${API_KEY}&with_genres=99',
-
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
 };
 
 export default requests;
+
+
+
+
+// const API_KEY = process.env.REACT_APP_API_KEY
+//"e48b1722d68d46e5996e073679a7a0f0";
+
+// https://api.themoviedb.org/3/movie/550?api_key=e48b1722d68d46e5996e073679a7a0f0
+
+// const requests = {
+
+// fetchTrending: '/trending/all/week?api_key=${API_KEY}&language=en-US',
+// fetchNetflixOriginals: '/discover/tv?api_key=${API_KEY}&with_networks=213',
+// fetchTopRatedMovies: '/movie/top_rated?api_ key=S{API KEY}51anguagesen-US', 
+// fetchActionMovies: '/discover/movie?api_key=${API_KEY}&with_genres=28', 
+// fetchComedyMovies: '/discover/movie?api_key=${API_KEY}&with_genres=35',
+// fetchHorrorMovies: '/discover/movie?ap_key=${API_KEY}with_genres=27', 
+// fetchRomanceMovies: '/discover/movie?api_key=${API_KEY}&with_genres=10749', 
+// fetchDocumentaries: '/discover/movie?ap1_key=${API_KEY}&with_genres=99',
+
+// };
+
+// export default requests;
